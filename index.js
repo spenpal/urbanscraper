2e8957bbfa3b98843c8145391339eb41577a0b00
Sanjeev Penupala <sanjeev.penupala@gmail.com>
2020-03-07 22:55:55 +0000

    Updated index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import Required Packages
+// Import axios
+// Import readline
+// Import fs
+
+// Create readOption Object
+
+// Initalize The Reader And Create The Interface
+
+// Prompt User For Input
+
+// Create Function called "getData"
+// Parameters = "term"
+// Urban Dictionary API: http://urbanscraper.herokuapp.com/
+// 3 Endpoints Total: Gonna Use 2 Of Them Today
+// http://urbanscraper.herokuapp.com/define/${term}
+// http://urbanscraper.herokuapp.com/search/${term}
